11409f736f1baeae56927af5a085994babc0ed04
kflinderman <kenny.linderman@gmail.com>
2018-01-18 22:32:28 -0500

    changed domain to work in 0.61.1
    All I did for my instance was change the domain from lightpack to light and I haven't had any issues yet.

--- lightpack.py
+++ lightpack.py
@@ -10,7 +10,7 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'lightpack'
+DOMAIN = 'light'
 ATTR_NAME = 'profile'
 
 # Validation of the user's configuration
